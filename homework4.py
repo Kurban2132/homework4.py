c07124121a20371005b3cb7c7c73ddf49c35affa
Kurban2132 <k.kokanov@yandex.ru>
2024-07-23 17:27:22 +0300

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,12 +1,10 @@
-name = "my_string"
+my_string = input("Как ваше имя: ")
 my_string = input("Введите количество символов: ")
 print("my_string".upper())
-print("my_string".lower())
-print("my_string".replace(" ", "#"))
-print(name[0])
-print(name[-1])
-
-
+print("me_string".lower())
+print("my_string".replace(" " , "#"))
+print("my_string"[0])
+print("my_string"[-1])
 
 
 
