6fe437631ec47431dd11df444fd835a5ded1a642
Kurban2132 <k.kokanov@yandex.ru>
2024-07-23 13:22:26 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,16 @@
+name = "my_string"
+my_string = input("Введите количество символов: ")
+print("my_string".upper())
+print("my_string".lower())
+print("my_string".replace(" ", "#"))
+print(name[0])
+print(name[-1])
+
+
+
+
+
+
+
+
+
