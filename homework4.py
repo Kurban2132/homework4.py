97f81e94aa77582f9cc8690b1bcadae631a1b7ab
Kurban2132 <k.kokanov@yandex.ru>
2024-07-24 10:05:46 +0300

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,9 +1,9 @@
 my_string = input("Как ваше имя: ")
 A = len("my_string")
 print(A)
-print("my_string".upper())
-print("me_string".lower())
-print("my_string".replace(" " , "#"))
+print( ("Как ваше имя".upper()))
+print("Как ваше имя".lower())
+print("Как ваше имя".replace(" " , "#"))
 print("my_string"[0])
 print("my_string"[-1])
 
